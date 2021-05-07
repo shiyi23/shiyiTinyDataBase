61c5a062d6b5f0a03848e8e5ccdcc1a0151dfeed
ac23 <3045712342@qq.com>
2021-05-07 18:02:21 +0800

    代码更新

--- src/main/org/huangshiyi/KVEngine/Bytes.h
+++ src/main/org/huangshiyi/KVEngine/Bytes.h
@@ -34,7 +34,7 @@ public:
 
     static long toLong(std::deque<unsigned char>& a);
 
-    static std::deque<unsigned char> slice(std::deque<unsigned char>& buf, int& offset, int& len, int& buf_len);
+    static std::deque<unsigned char> slice(std::deque<unsigned char>& buf, int offset, int len);
 
     static int hash(std::deque<unsigned char>& key, int& key_len);
 
--- src/main/org/huangshiyi/KVEngine/DiskFile.h
+++ src/main/org/huangshiyi/KVEngine/DiskFile.h
@@ -84,13 +84,13 @@ private:
     void close();
 };
 
-//实现内部类
+//实现内部类BlockMeta
 class DiskFile::BlockMeta
 {
 public:
     BlockMeta()
     {
-        const int OFFSET_SIZE = 8;
+        int OFFSET_SIZE = 8;
         const int SIZE_SIZE = 8;
         const int BF_LEN_SIZE = 4;
     }
@@ -101,13 +101,110 @@ public:
         this->blockSize = size;
         this->bloomFilter = bloomFilter;
     }
-    KeyValue getLastKV();
-    long getBlockOffset();
-    long getBlockSize();
-    std::deque<unsigned char> getBloomFilter();
-    int getSerializeSize();
-    std::deque<unsigned char> toBytes();
-    static BlockMeta parseFrom(std::deque<unsigned char>& buf, int offset);
+    KeyValue getLastKV() 
+    {
+        return this->lastKV;
+    }
+    long getBlockOffset()
+    {
+        return this->blockOffset;
+    }
+    long getBlockSize()
+    {
+        return this->blockSize;
+    }
+    std::deque<unsigned char> getBloomFilter()
+    {
+        return this->bloomFilter;
+    }
+    int getSerializeSize()
+    {
+        return lastKV.getSerializeSize() + OFFSET_SIZE + SIZE_SIZE + BF_LEN_SIZE + bloomFilter.size();
+    }
+    std::deque<unsigned char> toBytes()
+    {
+        std::deque<unsigned char> bytes(getSerializeSize());
+        int pos = 0;
+        auto posForCopy = bytes.begin();
+
+        //Encode last kv
+        std::deque<unsigned char> kvBytes = lastKV.toBytes();
+        for (int i = 0; i < pos; i++)
+        {
+            ++posForCopy;
+        }
+        std::copy_n(kvBytes.begin(), kvBytes.size(), posForCopy);
+        pos += kvBytes.size();
+
+        //Encode blockOffset
+        std::deque<unsigned char> offsetBytes = Bytes::toBytes(blockOffset);
+        for (int i = 0; i < pos; i++)
+        {
+            ++posForCopy;
+        }
+        std::copy_n(offsetBytes.begin(), offsetBytes.size(), posForCopy);
+        pos += offsetBytes.size();
+
+        //Encode blockSize
+        std::deque<unsigned char> sizeBytes = Bytes::toBytes(blockSize);
+        for (int i = 0; i < pos; i++)
+        {
+            ++posForCopy;
+        }
+        std::copy_n(sizeBytes.begin(), sizeBytes.size(), posForCopy);
+        pos += sizeBytes.size();
+
+        //Encode length of bloom filter
+        std::deque<unsigned char> bfLenBytes = Bytes::toBytes(bloomFilter.size());
+        for (int i = 0; i < pos; i++)
+        {
+            ++posForCopy;
+        }
+        std::copy_n(bfLenBytes.begin(), bfLenBytes.size(), posForCopy);
+        pos += bfLenBytes.size();
+
+        //Encode bytes of bloom filter
+        for (int i = 0; i < pos; i++)
+        {
+            ++posForCopy;
+        }
+        std::copy_n(bloomFilter.begin(), bloomFilter.size(), posForCopy);
+        pos += bloomFilter.size();
+        
+        if (pos != bytes.size())
+        {   
+            std::cout << "pos = " << pos << "\n";
+            std::cout << "bytes.size() = " << bytes.size() << "\n";
+            throw "pos should be equal to the size of bytes";
+        }
+        return bytes;
+    }
+
+    static BlockMeta parseFrom(std::deque<unsigned char>& buf, int offset)
+    {
+        int pos = offset;
+
+        //Decode last key value
+        KeyValue lastKV = KeyValue::parseFrom(buf, offset);
+        pos += lastKV.getSerializeSize();
+
+        //Decode block blockOffset
+        std::deque<unsigned char> temp0 = Bytes::slice(buf, pos, OFFSET_SIZE);
+        long blockOffset = Bytes::toLong(temp0);
+        pos += OFFSET_SIZE;
+
+        //Decode block blockSize
+        std::deque<unsigned char> temp1 = Bytes::slice(buf, pos, SIZE_SIZE);
+        pos += SIZE_SIZE;
+
+        //Decode blockSize of block's bloom filter
+        std::deque<unsigned char> temp2 = Bytes::slice(buf, pos, BF_LEN_SIZE);
+        int bloomFilterSize = Bytes::toInt(temp2);
+        pos += BF_LEN_SIZE;
+
+        //Decode bytes of block's bloom filter
+        
+    }
     int compareTo(BlockMeta o);
     virtual ~BlockMeta();
 
